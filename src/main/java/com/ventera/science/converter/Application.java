f1cb5df3b44c77a6e6dea6bbc3bc8249a39db7eb
Sariph Shrestha <sariph@sariphs-mbp-2.mynetworksettings.com>
2022-04-10 20:51:45 -0400

    Added Test Cases for Java

--- src/main/java/com/ventera/science/converter/Application.java
+++ src/main/java/com/ventera/science/converter/Application.java
@@ -6,11 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class Application {
-  @GetMapping(value = "/{path:[^\\.]*}")
-  public String redirect() {
-    return "forward:/";
-  }
-
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
